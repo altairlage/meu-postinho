3d0c7c3d4ea6aa24d80c1effd708886478c941d7
Vitor Cometti <vitor.cometti@ntconsult.com.br>
2025-09-03 21:18:45 -0300

    Inclusao do servico de agendamento de visitas e producer kafka

---
+++ meu-postinho-api/src/main/java/com/meu_postinho_api/MeuPostinhoApiApplication.java
@@ -0,0 +1,13 @@
+package com.meu_postinho_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeuPostinhoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeuPostinhoApiApplication.class, args);
+	}
+
+}
---
+++ meu-postinho-api/src/test/java/com/meu_postinho_api/MeuPostinhoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meu_postinho_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeuPostinhoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
